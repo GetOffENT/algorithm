8dd2b6898d0e018d605dfff49207fa1f2d27cd02
Yuxian Wu <12212614@mail.sustech.edu.cn>
2025-08-14 21:36:36 +0800

    redo4

---
+++ practice/leetcode/src/hot100_redo3/binarytree/TreeNode.java
@@ -0,0 +1,26 @@
+package hot100_redo3.binarytree;
+
+/**
+ * @author Will
+ * @version 1.0
+ * @Description: TODO
+ * @Create: 2024-09-13 11:43
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
